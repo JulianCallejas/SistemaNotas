20ce53e8548a15c4f71446e218022de6b060ad7c
a3cortes <a3cortes@gmail.com>
2022-06-07 17:09:57 -0500

    Update ControladorNotas.py

--- ControladorNotas.py
+++ ControladorNotas.py
@@ -1,17 +1,16 @@
 #Modulo Central de la app, que integra y ejecuta los modulos
-
+#Modulo Central de la app, que integra y ejecuta los modulos
 import pandas as pd
-
 def CargaNotasCSV():
     # Leyendo archivo csv
     try:
         df1 = pd.read_csv("Notas.csv",dtype=str,sep = ';')
         return df1,1
     except FileNotFoundError:
-        print("Archivo no encontrado.")    
+        return "Archivo no encontrado.", 0
     except pd.errors.EmptyDataError:
-        print("No hay informacion que leer")   
+        return "No hay informacion que leer", 0  
     except pd.errors.ParserError:
-        print("Error de analisis")  
+        return "Error de analisis", 0  
     except Exception:
-        print("Alguna otra Exepcion")   
+        return "Alguna otra Exepcion", 0   
