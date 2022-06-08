1c1596c1ec4c48e8704ec608616433a116ca6d92
JulianCallejas <105081390+JulianCallejas@users.noreply.github.com>
2022-06-07 21:48:49 -0500

    Arreglo/Mejora funciones Cargadatos

--- ControladorNotas.py
+++ ControladorNotas.py
@@ -1,10 +1,10 @@
 #Modulo Central de la app, que integra y ejecuta los modulos
 import pandas as pd
-
+#Se crean las funciones para cargar datos
 def CargaNotasCSV():
     # Leyendo archivo csv
     try:
-        df1 = pd.read_csv("Notas.csv",dtype=str,sep = ';')
+        df1 = pd.read_csv("Notas.csv",dtype=str,sep = ';', index_col = "IdNota")
         return df1,1
     except FileNotFoundError:
         return "Archivo no encontrado.", 0
@@ -15,9 +15,10 @@ def CargaNotasCSV():
     except Exception:
         return "Alguna otra Exepcion", 0     
 
+
 def CargaMateriasCSV():
     try:
-        df1 = pd.read_csv("Materias.csv",dtype=str,sep = ';')
+        df1 = pd.read_csv("Materias.csv",dtype=str,sep = ';', index_col = "IDMateria")
         return df1,1
     except FileNotFoundError:
         return "Archivo no encontrado.", 0
@@ -29,24 +30,9 @@ def CargaMateriasCSV():
         return "Alguna otra Exepcion", 0       
 
 
-def CargaMateriasCSV():
-    try:
-        df1 = pd.read_csv("Materias.csv",dtype=str, sep=';')
-        df1 = df1.set_index('IDMateria')
-        return df1,1
-    except FileNotFoundError:
-        return "Archivo no encontrado.", 0
-    except pd.errors.EmptyDataError:
-        return "No hay informacion que leer", 0  
-    except pd.errors.ParserError:
-        return "Error de analisis", 0  
-    except Exception:
-        return "Alguna otra Exepcion", 0         
-
 def CargaProfesoresCSV():
     try:
-        df1 = pd.read_csv("Profesores.csv",dtype=str,sep=';')
-        df1 = df1.set_index('IDProfesor')
+        df1 = pd.read_csv("Profesores.csv",dtype=str,sep=';', index_col = "IDProfesor")
         return df1,1
     except FileNotFoundError:
         return "Archivo no encontrado.", 0
@@ -60,8 +46,7 @@ def CargaProfesoresCSV():
 
 def CargaEstudiantesCSV():
     try:
-        df1 = pd.read_csv("Estudiantes.csv",dtype=str,sep=';')
-        df1 = df1.set_index('IDEstudiante')
+        df1 = pd.read_csv("Estudiantes.csv",dtype=str,sep=';', index_col = "IDEstudiante")
         return df1,1
     except FileNotFoundError:
         return "Archivo no encontrado.", 0
@@ -72,10 +57,10 @@ def CargaEstudiantesCSV():
     except Exception:
         return "Alguna otra Exepcion", 0 
 
+
 def CargaGruposCSV():
     try:
-        df1 = pd.read_csv("Grupos.csv",dtype=str,sep=';')
-        df1 = df1.set_index('IDGrupo')
+        df1 = pd.read_csv("Grupos.csv",dtype=str,sep=';', index_col = "IDGrupo")
         print(df1)
         return df1,1
     except FileNotFoundError:
